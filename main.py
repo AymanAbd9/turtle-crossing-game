e04efae8f5b65d76689e72720273d36e1ebdd122
Ayman Abd <ayman.abd889@gmail.com>
2022-06-22 22:56:35 +0300

    detecting if player collides with cars

--- main.py
+++ main.py
@@ -21,5 +21,14 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
+    # Detect if player collides with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+
+
+screen.mainloop()
+
 
 
