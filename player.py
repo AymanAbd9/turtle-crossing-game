97ea1bfe29c587372551f8105c25593893f90672
Ayman Abd <ayman.abd889@gmail.com>
2022-06-22 22:24:58 +0300

    init

---
+++ player.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("turtle")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move_up(self):
+        x = self.xcor()
+        y = self.ycor() + MOVE_DISTANCE
+        self.goto(x, y)
