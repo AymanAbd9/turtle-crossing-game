241de954e9e641947ac4e4d04e725f313fca667e
Ayman Abd <ayman.abd889@gmail.com>
2022-06-24 21:04:48 +0300

    scoreboard to display level of the game

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 screen.onkey(player.move_up, "Up")
@@ -24,12 +26,15 @@ while game_is_on:
     # Detect if player collides with cars
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
+            scoreboard.game_over()
             game_is_on = False
+
 
     # Detect if player reached the top 
     if player.ycor() > 300:
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
     # Removing a car when it reaches the left side
     car_manager.remove_cars()        
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,28 @@
-FONT = ("Courier", 24, "normal")
+from tkinter import CENTER
+from turtle import Turtle
+
+FONT = ("Courier", 14, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+        
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-240, 270)
+        self.write(f"Level: {self.level}", align=CENTER, font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=CENTER, font=FONT)
